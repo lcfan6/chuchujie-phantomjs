f1d0af8c5bcf03b88a13d37eaaca5eea04d6ba21
刘超凡 <lcfme@sina.com>
2017-12-31 04:24:19 +0800

    tj

---
+++ capturePackage/node-call-phantom.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var fs = require('fs');
+var childProcess = require('child_process');
+var phantomjs = require('phantomjs-prebuilt');
+var binPath = phantomjs.path;
+
+module.exports = function(webUrl, cb) {
+    if (typeof cb !== 'function') {
+        throw new Error('node-call-phantom require a callback function');
+    }
+    if (!webUrl) {
+        cb(new Error('url is required'));
+        return;
+    }
+    if (!/^w+:\/\//.test(webUrl)) {
+        webUrl = 'http://' + webUrl;
+    }
+    var childArgs = [path.resolve(__dirname, 'phantom-capture.js'), webUrl];
+
+    childProcess.execFile(
+        binPath,
+        childArgs,
+        { timeout: 10000, maxBuffer: 10 * 1024 * 1024 },
+        function(err, stdout, stderr) {
+            if (err) {
+                cb(err);
+                return;
+            }
+            var img = Buffer.from(stdout, 'base64');
+            cb(null, img);
+            // var f = fs.createWriteStream(
+            //     path.resolve(
+            //         __dirname,
+            //         '..',
+            //         'imgs',
+            //         Math.random()
+            //             .toString(32)
+            //             .substr(2)
+            //     ) + '.png'
+            // );
+            // f.write(img);
+        }
+    );
+};
---
+++ index.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var url = require('url');
+var express = require('express');
+// var formidable = require('formidable');
+var nodeCallPhantom = require('./capturePackage/node-call-phantom');
+
+var app = express();
+
+app.get('/api/capture_webpage', function(req, res) {
+    var query = url.parse(req.url, true).query;
+    var type = query.type;
+    var webUrl = query.url;
+    nodeCallPhantom(webUrl, function(err, img) {
+        if (type === 'base64') {
+            var reply = {
+                error: 0,
+                data: {
+                    img: ''
+                }
+            };
+            if (err) {
+                reply.error = 1;
+                res.send(reply);
+                return;
+            }
+            img = 'data:image/png;base64,' + img.toString('base64');
+            reply.data.img = img;
+            res.send(reply);
+            return;
+        }
+        if (err) {
+            res.status(404).end();
+        }
+        res.send(img);
+    });
+});
+
+http.createServer(app).listen(process.env.PORT || 3000);
