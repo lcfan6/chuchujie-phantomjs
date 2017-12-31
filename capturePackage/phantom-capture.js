8be0ba2294366b461ed9da28f2127a7c2303dfbe
刘超凡 <lcfme@sina.com>
2017-12-31 12:24:39 +0800

    done

--- capturePackage/phantom-capture.js
+++ capturePackage/phantom-capture.js
@@ -11,13 +11,9 @@ page.customHeaders = {
 
 page.open(webUrl, function(status) {
     if (status === 'success') {
-        setTimeout(function () {
-            var base64 = page.renderBase64('png');
+        var base64 = page.renderBase64('png');
         write(base64);
-        page.close();
-    phantom.exit();
-        }, 5000);
-        
     }
-    
+    page.close();
+    phantom.exit();
 });
