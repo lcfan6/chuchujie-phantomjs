458f908dfd0263f82dfb46a54bf249dd2fb608a5
刘超凡 <lcfme@sina.com>
2018-01-01 20:27:07 +0800

    done

--- capturePackage/phantom-capture.js
+++ capturePackage/phantom-capture.js
@@ -9,7 +9,7 @@ page.customHeaders = {
         'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1'
 };
 
-page.open(webUrl, function(status) {
+page.open('https://www.baidu.com', function(status) {
     if (status === 'success') {
         var base64 = page.renderBase64('png');
         write(base64);
