a641a64e20bebde8496105770561ff5c7e7f2495
刘超凡 <lcfme@sina.com>
2018-01-01 23:52:24 +0800

    done

--- capturePackage/phantom-capture.js
+++ capturePackage/phantom-capture.js
@@ -9,13 +9,11 @@ page.customHeaders = {
 };
 
 page.open(webUrl, function(status) {
-    setTimeout(function() {
-        if (status === 'success') {
-            var base64 = page.renderBase64('png');
-            // console.log(base64);   
-            system.stdout.write(base64);
-        }
-        page.close();
-        phantom.exit();
-    }, 3000);
+    if (status === 'success') {
+        var base64 = page.renderBase64('png');
+        // console.log(base64);
+        system.stdout.write(base64);
+    }
+    page.close();
+    phantom.exit();
 });
